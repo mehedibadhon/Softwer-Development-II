065c7b9e592538cae9990760b8fa885f2381ba95
mehedibadhon <48272619+mehedibadhon@users.noreply.github.com>
2022-04-04 17:24:35 +0600

    index uplode

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose=require('mongoose');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 
 const app = express();
 const PORT = 3000;
+const url='mongodb://127.0.0.1:27017/assignment-2';
+const connect=mongoose.coonect(url);
+
+connect.then((db)=>{
+    console.log("Connected correctly to server");
+},(err)=>{console.log(err);});
 
 app.get('/', (req, res) => {
     res.send(`<h2> This is home page</h2> </br> </br>  <h3> Name : Mehedi Hasan </br> Id : 1710876109</h3>`);
